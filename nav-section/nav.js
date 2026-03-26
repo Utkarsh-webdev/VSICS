65990d9f574c278c79309b7e020916bdd82dad30
Your Name <utkarshjha832@gmail.com>
2026-03-26 22:16:21 +0530

    feat: Add gap to flex items in the navigation for improved spacing

--- nav-section/nav.js
+++ nav-section/nav.js
@@ -2,9 +2,11 @@ const toggle = document.querySelector(".menu-toggle");
 const nav = document.querySelector(".main-nav");
 
 toggle.addEventListener("click", () => {
+  toggle.classList.toggle("active");   // 🔥 animation
   nav.classList.toggle("nav-open");
 });
 
+// dropdowns
 const dropdowns = document.querySelectorAll(".has-dropdown");
 
 dropdowns.forEach(dropdown => {
@@ -21,6 +23,7 @@ dropdowns.forEach(dropdown => {
   });
 });
 
+// close all
 document.addEventListener("click", () => {
   dropdowns.forEach(d => d.classList.remove("active"));
 });
